1892cc94d241fa7feac739996e66fdf0987e3e15
Gabriel Blanco <geblarot96@gmail.com>
2021-08-25 13:04:41 -0500

    Change Application Extension

--- src/co/edu/unbosque/model/persistence/utils/FileMaker.java
+++ src/co/edu/unbosque/model/persistence/utils/FileMaker.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class FileMaker {
 
     public void escribir(Object obj, String nombre) throws IOException {
-        File f = new File(nombre+".cer");
+        File f = new File(nombre+".celuventas");
         FileOutputStream fos = new FileOutputStream(f);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(obj);
@@ -15,7 +15,7 @@ public class FileMaker {
 
     public Object leer(String nombre) throws IOException, ClassNotFoundException {
         Object obj = null;
-        File f = new File(nombre+".cer");
+        File f = new File(nombre+".celuventas");
         FileInputStream fis = new FileInputStream(f);
         try {
             ObjectInputStream ois = new ObjectInputStream(fis);
